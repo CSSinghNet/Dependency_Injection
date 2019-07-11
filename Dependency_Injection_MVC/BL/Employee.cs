9e8d1f0f5d54bcf61f25c158350ca812fef21298
chandrashekhar singh <synthesis_chandrashekhar@hotmail.com>
2019-07-11 12:48:15 +0530

    Dependency Injection using .Net MVC

---
+++ Dependency_Injection_MVC/BL/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace BL
+{
+    public class Employee: IEmployee
+    {
+        public int GetTotalStudents()
+        {
+            return 10;
+        }
+    }
+}
---
+++ Dependency_Injection_MVC/Dependency_Injection_MVC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BL;
+namespace Dependency_Injection_MVC.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private IEmployee employee = null;
+        private IStudent student = null;
+
+        public HomeController(IEmployee _employee,IStudent _student)
+        {
+            employee = _employee;
+            student = _student;
+        }
+
+        public ActionResult Index()
+        {
+            int count = employee.GetTotalStudents();
+            bool data = student.IsStudent();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
